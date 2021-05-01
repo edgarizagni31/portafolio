d8ab949653881dc930249bbaba521d2f6a5af785
edgarizagni31 <ochoaEdgar3108@gmail.com>
2021-04-30 21:53:45 -0500

    add contact component

---
+++ src/components/FormItem.js
@@ -0,0 +1,37 @@
+import React, { useRef } from 'react'
+
+
+export const FormItem = ( { id, title } )  => {
+    const label = useRef(null);
+    const input = useRef(null);
+
+    const handleActive = ( ) => {
+        label.current.classList.add('contact__label--active');
+    }
+
+    const handleDesactive = () => {
+        if ( input.current.value == "") {
+            label.current.classList.remove('contact__label--active');
+        }
+    }
+
+    return (
+        <div>
+            <label 
+                htmlFor = {id}  
+                className = 'contact__label'
+                ref = { label }
+            >
+                { title }
+            </label>
+            <input  
+                id = {id} 
+                className = 'contact__input'
+                autoComplete = 'off'
+                onFocus = { handleActive }
+                onBlur = { handleDesactive }
+                ref = { input }
+            />
+        </div>
+    )
+}
--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { FormItem } from '../components/FormItem'
+
+import '../styles/contact.css';
 
 export const Contact = () => {
     return (
-        <div>
-            <h1>Contact</h1>         
-        </div>
+        <section className = 'contact'>
+            <div>
+                <h2 className = 'contact__title'>!hablemos!</h2>   
+                <form className = 'contact__form' >
+                    <FormItem id = 'name' title = 'Nombre' />
+                    <FormItem id = 'email' title = 'Email' />
+                    <FormItem id = 'subject'  title = 'Asunto'/>
+                    <FormItem id = 'message' title = 'Mensaje' />
+                    <button className = 'contact__btn' >Enviar Mensaje</button>
+                </form>    
+            </div>
+        </section>
     )
 }
