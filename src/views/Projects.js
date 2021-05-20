e46875aa459e9a86b09b32e54b3e05bd653a0861
edgarizagni31 <ochoaEdgar3108@gmail.com>
2021-05-20 18:12:04 -0500

    projects view

--- src/views/Projects.js
+++ src/views/Projects.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { ProjectsGrid } from '../components/ProjectsGrid'
 
+import '../styles/projects.css';
+
 export const Projects = () => {
     return (
-        <div>
-            <h2>Projects</h2>  
+        <section className = 'projects'>
+            <h2 className = 'projects__title'>Proyectos</h2>  
             <ProjectsGrid />       
-        </div>
+        </section>
     )
 }
